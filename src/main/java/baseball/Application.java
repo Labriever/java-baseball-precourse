0970f4be9031caf7c56c3d90520b5c00ed801b0d
Labriever <liveinmars2018@gmail.com>
2024-07-10 14:39:44 +0900

    세자리 숫자의 난수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+    	Scanner sc = new Scanner(System.in);
+    	
     }
+    
+    // 정답 number를 생성하는 메소드 입니다.
+    private static int number() {
+    	return 0;
+    }
+    
+    
 }
