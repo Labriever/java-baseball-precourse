ac6c47ad7caec14bf898c7c8f6d42264595688c6
Labriever <liveinmars2018@gmail.com>
2024-07-10 15:15:18 +0900

    세자리 수 난수 생성 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,22 +6,34 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-    	Scanner sc = new Scanner(System.in);
     	
+
     }
     
     // 정답 number를 생성하는 메소드 입니다.
     private static int number() {
+    	//int num = RandomNum9to1();
     	int[] num = {1,2,3,4,5,6,7,8,9};
-    	int baseball = 0;
+    	int check = 0;
+    	StringBuilder sb = new StringBuilder();
+    	while(check < 3) {
+    		int i = RandomNum9to1();
+    		if(num[i] != 0) {
+    			check++;
+    			sb.append(num[i]);
+    			num[i] = 0;
+    		}
+    	}
     	
-    	return 0;
+    	int baseball = Integer.parseInt(sb.toString());
+    	
+    	return baseball;
     	
     }
     
     // 9에서 1까지의 랜덤한 숫자를 반환하는 메소드입니다.
     private static int RandomNum9to1() {
-    	return (int)(Math.random()*9)+1;
+    	return (int)(Math.random()*9);
     }
     
     
