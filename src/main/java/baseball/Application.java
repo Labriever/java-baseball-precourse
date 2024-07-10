cddd5d3dc6738a3e617d266b9427029cb1558e55
Labriever <liveinmars2018@gmail.com>
2024-07-10 15:00:38 +0900

    9에서 1까지의 랜덤한 숫자를 반환하는 메소드 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,9 +12,16 @@ public class Application {
     
     // 정답 number를 생성하는 메소드 입니다.
     private static int number() {
+    	int[] num = {1,2,3,4,5,6,7,8,9};
+    	int baseball = 0;
     	
     	return 0;
     	
+    }
+    
+    // 9에서 1까지의 랜덤한 숫자를 반환하는 메소드입니다.
+    private static int RandomNum9to1() {
+    	return (int)(Math.random()*9)+1;
     }
     
     
